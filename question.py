d246d92fce71d324dd516f2452bd723169410b8d
naoya25 <onaoya2002@gmail.com>
2024-05-10 23:57:43 +0900

    fix

--- question.py
+++ question.py
@@ -4,21 +4,94 @@ class Question:
         self.q_str = (
             "800000000003600000070090200050007000000045700000100030001000068008500010090000400"
         )
-        self.ans = (
-            "812753649943682175675491283154237896369845721287169534521974368438526917796318452"
-        )
 
-    def set(self, q_str):
-        self.q_str = q_str
+    def set_q_str(self, s):
+        self.q_str = s
 
-    def str2list(self):
-        q_list = []
+    def display_s(self, s):
+        print("+-------+-------+-------+")
         for i in range(9):
-            row = list(map(int, list(self.q_str[i * 9 : i * 9 + 9])))
-            q_list.append(row)
-        return q_list
+            b1 = " ".join(map(lambda x: "." if x == "0" else x, s[i * 9 : i * 9 + 3]))
+            b2 = " ".join(map(lambda x: "." if x == "0" else x, s[i * 9 + 3 : i * 9 + 6]))
+            b3 = " ".join(map(lambda x: "." if x == "0" else x, s[i * 9 + 6 : i * 9 + 9]))
+            print(f"| {b1} | {b2} | {b3} |")
+            if i % 3 == 2:
+                print("+-------+-------+-------+")
 
-    # 9 x 9の2次元配列を返す
-    def get_q_list(self):
-        q_list = self.str2list()
-        return q_list
+    # self.q_strの全マスチェック
+    def check_all(self):
+        # 横
+        for i in range(9):
+            ss = self.q_str[i * 9 : (i + 1) * 9]
+            for j in range(1, 10):
+                if ss.count(str(j)) > 1:
+                    return False
+        # 縦
+        for i in range(9):
+            ss = [self.q_str[i % 9 + 9 * j] for j in range(9)]
+            for j in range(1, 10):
+                if ss.count(str(j)) > 1:
+                    return False
+        # 3 x 3
+        for i in range(9):
+            ss = [self.q_str[i // 3 * 27 + i % 3 * 3 + j // 3 * 9 + j % 3] for j in range(9)]
+            for j in range(1, 10):
+                if ss.count(str(j)) > 1:
+                    return False
+        return True
+
+    # sのi番目のみチェック
+    def check_i(self, s, i):
+        # 横
+        r = i // 9
+        ss = s[r * 9 : (r + 1) * 9]
+        for j in range(1, 10):
+            if ss.count(str(j)) > 1:
+                return False
+        # 縦
+        ss = [s[i % 9 + 9 * j] for j in range(9)]
+        for j in range(1, 10):
+            if ss.count(str(j)) > 1:
+                return False
+        # 3 x 3
+        ss = [s[i // 27 * 27 + i % 9 // 3 * 3 + j // 3 * 9 + j % 3] for j in range(9)]
+        for j in range(1, 10):
+            if ss.count(str(j)) > 1:
+                return False
+        return True
+
+    # 探索時に選択肢の少ないものの位置とその選択肢を計算
+    def find_next(self, s):
+        i, max_n, cannot_set = 0, 0, set()
+        for k in range(81):
+            if s[k] != "0":
+                continue
+
+            r_arr = list(s[k // 9 * 9 : k // 9 * 9 + 9])  # 横
+            c_arr = [s[k % 9 + 9 * j] for j in range(9)]  # 縦
+            b_arr = [s[k // 27 * 27 + k % 9 // 3 * 3 + j // 3 * 9 + j % 3] for j in range(9)]  # 3x3
+
+            now_set = set(r_arr + c_arr + b_arr)
+            n = len(now_set)
+            if max_n < n:
+                max_n = n
+                i = k
+                cannot_set = now_set
+        return i, set(map(str, range(1, 10))) - cannot_set
+
+    # dfsで解答作成
+    def get_ans(self):
+        ans = []
+        stack = [(0, self.q_str)]
+        while stack:
+            i, t = stack.pop()
+
+            if i == self.q_str.count("0"):
+                ans.append(t)
+                continue
+
+            k, next_options = self.find_next(t)
+            for j in next_options:
+                new_t = t[:k] + j + t[k + 1 :]
+                stack.append((i + 1, new_t))
+        return ans
--- v4.py
+++ v4.py
@@ -4,92 +4,19 @@ import time
 
 def main():
     q = Question()
-    q_str = q.q_str
+    q.set_q_str("800000000003600000070090200050007000000045700000100030001000068008500010090000400")
+    q.display_s(q.q_str)
 
-    if not check_all(q_str):
+    if not q.check_all():
         print("正しいナンプレを入力してね")
         return
 
-    # dfs
     s_t = time.time()
-    stack = [(0, q_str)]
-    while stack:
-        i, t = stack.pop()
-
-        if i == q_str.count("0"):
-            print(t)
-            print("正解" if t == q.ans else "不正解")
-            continue
-
-        k, next_options = find_next(t)
-        for j in next_options:
-            new_t = t[:k] + j + t[k + 1 :]
-            stack.append((i + 1, new_t))
-
+    ans = q.get_ans()
+    for a in ans:
+        q.display_s(a)
     e_t = time.time()
     print(f"タイム: {e_t - s_t}s")
-
-
-# 全マスチェック
-def check_all(s):
-    # 横
-    for i in range(9):
-        ss = s[i * 9 : (i + 1) * 9]
-        for j in range(1, 10):
-            if ss.count(str(j)) > 1:
-                return False
-    # 縦
-    for i in range(9):
-        ss = [s[i % 9 + 9 * j] for j in range(9)]
-        for j in range(1, 10):
-            if ss.count(str(j)) > 1:
-                return False
-    # 3 x 3
-    for i in range(9):
-        ss = [s[i // 3 * 27 + i % 3 * 3 + j // 3 * 9 + j % 3] for j in range(9)]
-        for j in range(1, 10):
-            if ss.count(str(j)) > 1:
-                return False
-    return True
-
-
-def check_i(s, i):
-    # 横
-    r = i // 9
-    ss = s[r * 9 : (r + 1) * 9]
-    for j in range(1, 10):
-        if ss.count(str(j)) > 1:
-            return False
-    # 縦
-    ss = [s[i % 9 + 9 * j] for j in range(9)]
-    for j in range(1, 10):
-        if ss.count(str(j)) > 1:
-            return False
-    # 3 x 3
-    ss = [s[i // 27 * 27 + i % 9 // 3 * 3 + j // 3 * 9 + j % 3] for j in range(9)]
-    for j in range(1, 10):
-        if ss.count(str(j)) > 1:
-            return False
-    return True
-
-
-def find_next(s):
-    i, max_n, cannot_set = 0, 0, set()
-    for k in range(81):
-        if s[k] != "0":
-            continue
-
-        r_arr = list(s[k // 9 * 9 : k // 9 * 9 + 9])  # 横
-        c_arr = [s[k % 9 + 9 * j] for j in range(9)]  # 縦
-        b_arr = [s[k // 27 * 27 + k % 9 // 3 * 3 + j // 3 * 9 + j % 3] for j in range(9)]  # 3x3
-
-        now_set = set(r_arr + c_arr + b_arr)
-        n = len(now_set)
-        if max_n < n:
-            max_n = n
-            i = k
-            cannot_set = now_set
-    return i, set(map(str, range(1, 10))) - cannot_set
 
 
 if __name__ == "__main__":
