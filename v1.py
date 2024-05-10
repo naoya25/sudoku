00ba292ef053668559fa8dea2aeedbac3aec9094
naoya25 <onaoya2002@gmail.com>
2024-05-10 16:52:06 +0900

    add v1

--- v1.py
+++ v1.py
@@ -1,9 +1,53 @@
-from question import q1_list, q1_str
+from question import Question
 
 
 def main():
-    print(q1_list)
-    return
+    q = Question()
+    q_str = q.q_str
+
+    if not check(q_str):
+        print("正しいナンプレを入力してね")
+        return
+
+    # dfs
+    stack = [(0, q_str)]
+    while stack:
+        i, t = stack.pop()
+
+        if i == 81:
+            print(t)
+            continue
+
+        if t[i] != "0":
+            stack.append((i + 1, t))
+            continue
+
+        for j in range(1, 10):
+            new_t = t[:i] + str(j) + t[i + 1 :]
+            if check(new_t):
+                stack.append((i + 1, new_t))
+
+
+def check(s):
+    # 横
+    for i in range(9):
+        ss = [s[i * 9 + j] for j in range(9)]
+        for j in range(1, 10):
+            if ss.count(str(j)) > 1:
+                return False
+    # 縦
+    for i in range(9):
+        ss = [s[i % 9 + 9 * j] for j in range(9)]
+        for j in range(1, 10):
+            if ss.count(str(j)) > 1:
+                return False
+    # 3 x 3
+    for i in range(9):
+        ss = [s[i // 3 * 27 + i % 3 * 3 + j // 3 * 9 + j % 3] for j in range(9)]
+        for j in range(1, 10):
+            if ss.count(str(j)) > 1:
+                return False
+    return True
 
 
 if __name__ == "__main__":
