ee97dd4f7c75fca68e77a27e9bb0f215aafc1558
naoya25 <onaoya2002@gmail.com>
2024-05-11 17:45:22 +0900

    fix

---
+++ analyze.py
@@ -0,0 +1,33 @@
+def main():
+    with open("questions.txt", "r") as f:
+        text = f.read()
+        data = text.split("\n")
+
+    # データ整形
+    questions = []
+    for q in data:
+        if not q.strip():
+            continue
+        q_arr = q.split("\t")
+        r_dict = {}
+        r_dict["id"] = int(q_arr[0])
+        r_dict["blank"] = int(q_arr[1].split(": ")[-1])
+        r_dict["ans"] = int(q_arr[2])
+        r_dict["question"] = int(q_arr[3])
+        questions.append(r_dict)
+    # for a in questions[:10]:
+    #     print(a)
+
+    distribution = {i: 0 for i in range(81)}
+    for q in questions:
+        distribution[q["blank"]] += 1
+
+    for k, v in distribution.items():
+        if not (50 <= k <= 64):
+            continue
+        print(f"blank: {k} --> {v}  {round(v / len(questions) * 100, 2)}%")
+    return
+
+
+if __name__ == "__main__":
+    main()
